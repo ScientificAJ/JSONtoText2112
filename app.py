1484a5212b7635ff9d7156eca0488e7e2b0ac083
AJ <110529039+ScientificAJ@users.noreply.github.com>
2022-08-05 12:41:12 +0530

    JSONtoText2112.py

---
+++ app.py
@@ -0,0 +1,24 @@
+print("""
+
+Hi, This Program is a file type converter
+
+""")
+x = input("Location of The File: ")
+y = input("Name of the New File Generated: ")
+z = input("Keywords to remove: ")
+
+with open(x) as file:
+    file.seek(0)
+    data = file.read()
+
+
+
+data = data.replace("'", "").replace("{", "").replace("}", "").replace('"', '').replace("[", "").replace("]", '\n').replace(r'\n', ' \n ').replace(z, "")
+print(data)
+
+
+f = open(y, "w")
+f.write(data)
+
+
+
