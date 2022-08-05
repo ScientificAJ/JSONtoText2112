fc6dfee0d0ac10fca51750c9c5db32e88d5b5a26
AJ <110529039+ScientificAJ@users.noreply.github.com>
2022-08-05 13:30:10 +0530

    JSONtoText2112.py

--- JSONtoText2112.py
+++ JSONtoText2112.py
@@ -11,14 +11,35 @@ with open(x) as file:
     file.seek(0)
     data = file.read()
 
+z = list(z.split(","))
+
+b = []
+
+for i in range(len(z)):
+    b.append(i)
+
+b = len(b)
+c = []
+for i2 in range(b):
+    c.append("")
+
+dictionary = dict(zip(z, c))
 
 
-data = data.replace("'", "").replace("{", "").replace("}", "").replace('"', '').replace("[", "").replace("]", '\n').replace(r'\n', ' \n ').replace(z, "")
+
+print(z)
+
+data = data.replace("'", "").replace("{", "").replace("}", "").replace('"', '').replace("[", "").replace("]", '\n').replace(r'\n', ' \n ')
+
+for index1, index2 in dictionary.items():
+    data = data.replace(index1, index2)
+
 print(data)
 
 
 f = open(y, "w")
 f.write(data)
+
 
 
 
